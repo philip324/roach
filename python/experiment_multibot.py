0f7b5d2b917b668fceae1e614dbc63344de2d232
Andrew Pullin <pullin@berkeley.edu>
2014-12-14 00:12:47 -0800

    Edits to python to reflect new imageproc-settings repo.

--- python/experiment_multibot.py
+++ python/experiment_multibot.py
@@ -10,6 +10,10 @@ The main function will send all the setup parameters to the robots, execute defi
 from lib import command
 import time,sys,os,traceback
 import serial
+
+# Path to imageproc-settings repo must be added
+sys.path.append(os.path.dirname("../../imageproc-settings/"))
+sys.path.append(os.path.dirname("../imageproc-settings/"))      # Some projects have a single-directory structure
 import shared
 
 from velociroach import *
